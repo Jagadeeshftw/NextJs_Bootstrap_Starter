f91709e8dbee5eb82f566484bf6cfbb793aeffbf
Jagadeeshftw <jagadeesh26062002@gmail.com>
2024-06-13 19:10:00 +0530

    Add a resolve.alias configuration for Bootstrap

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,21 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+const nextConfig = {
+  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+    // Add a resolve.alias configuration to the existing Webpack config
+    config.resolve.alias["~bootstrap"] = path.resolve(
+      __dirname,
+      "node_modules/bootstrap"
+    );
+
+    // Important: return the modified config
+    return config;
+  },
+};
 
 export default nextConfig;
